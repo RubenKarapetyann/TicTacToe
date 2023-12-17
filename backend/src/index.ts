33593abda0e882ba3005e8b2ee96d461a9a658b0
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-12-17 15:17:05 +0400

    created connection pool for database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,18 @@
 import express, { Request, Response } from "express"
 import { AUTH, TEST } from "../constants/routes-constants"
 import authentication from "./authentication/layout"
+import pool from "../database/pool"
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(AUTH,authentication)
 
-app.get(TEST,(req : Request,res : Response)=>{
+app.get(TEST,async (req : Request,res : Response)=>{
+    const result = await pool.query("SELECT * FROM users")
     res.json({
-        access : true
+        access : true,
+        data : result[0]
     })
 })
 
