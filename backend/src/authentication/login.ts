0e20dcdfe83ddf69a3eab4a5a2b3895e4d28e0a0
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-12-19 01:27:33 +0400

    created login route that generate jwt token if username and password are correct

--- backend/src/authentication/login.ts
+++ backend/src/authentication/login.ts
@@ -1,5 +1,25 @@
-import express from "express"
+import express, { Request, Response } from "express"
+import { getUserBy } from "../../utils/database"
+import { compare } from "../../utils/hash"
+import { generateJwtToken } from "../../utils/jwt"
 
 const router = express.Router()
+
+router.post("/",async (req: Request, res: Response)=>{
+    const { name, password } = req.body
+    const user = await getUserBy("name", name)
+
+    if(!user){
+        return res.status(404).json({access : false, message : "username is incorrect"})
+    }
+
+    const correctPassword = await compare(password, user.password)
+    if(!correctPassword){
+        return res.status(401).json({access : false, message : "password is incorrect"})
+    }
+
+    const token = generateJwtToken(user.name, user.id)
+    return res.status(200).json({access : true, message : "ok", token})
+})
 
 export default router
--- backend/utils/hash.ts
+++ backend/utils/hash.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt"
 import { HASH_SALT } from "../constants/utils"
 
-export const hash = async (password : string)=> await bcrypt.hash(password, HASH_SALT)
+export const hash = async (password : string)=> await bcrypt.hash(password.toString(), HASH_SALT)
 
-export const compare = async (password : string, hash : string)=> await bcrypt.compare(password, hash)
+export const compare = async (password : string, hash : string)=> await bcrypt.compare(password.toString(), hash)
