5fa3c77f455f77f2bf701e1ca32d55c3a87f2b07
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-12-22 21:08:30 +0400

    added global styles and color palette

---
+++ mobile/constants/styles/palette-constants.ts
@@ -0,0 +1,10 @@
+const PALETTE = {
+    primary : "#FF4343",
+    background : "#fffff",
+    accent : "#FFBAOO",
+    secondary : "#1B3364",
+    text : "black",
+    border : "#32373b",
+}
+
+export default PALETTE
---
+++ mobile/styles/global.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const gStyles = StyleSheet.create({
+    container : {
+        padding : 0,
+        margin : 0,
+        flex : 1
+    }
+})
+
+export default gStyles
