79db2789194b699a64b7b258fbf4582eac1ad5f8
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-12-24 12:22:35 +0400

    added simple styles for register screen

--- mobile/screens/root-screens/Register.tsx
+++ mobile/screens/root-screens/Register.tsx
@@ -1,10 +1,40 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import RegisterForm from "../../components/form/RegisterForm";
+import { RegisterProps } from "../../types/navigation/RootStackTypes";
+import { LOGIN } from "../../constants/navigation/root-stack-constants";
+import PALETTE from "../../constants/styles/palette-constants";
 
-export default function Register(){
+export default function Register({ navigation }: RegisterProps){
+    const goToLogin = ()=> navigation.navigate(LOGIN)
+
     return (
-        <View>
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.titleText}>Register</Text>
+            </View>
             <RegisterForm/>
+            <TouchableOpacity style={styles.loginButton} onPress={goToLogin}>
+                <Text>already have account? </Text>
+                <Text style={styles.loginText}>login!</Text>
+            </TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    loginText : {
+        color : PALETTE.secondary,
+    },
+    loginButton : {
+        flexDirection : "row",
+        marginTop : 20
+    },
+    container : {
+        alignItems : "center",
+        height : "100%",
+        justifyContent : "center"
+    },
+    titleText : {
+        fontSize : 23
+    }
+})
